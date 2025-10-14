a2bf1c4641dcda12f345c7effc905069828c1cc5
mrmorgan-i <misraelkelly@gmail.com>
2025-10-14 16:13:07 -0500

    add post endpoint to api; add form for accepting and mutating data

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -6,3 +6,16 @@ export async function GET () {
     const fetchedTasks = await db.select().from(tasks);
     return NextResponse.json(fetchedTasks);
 }
+
+export async function POST (request: Request) {
+    const newTask = await request.json();
+
+    const createdTask = await db.insert(tasks).values({
+        title: newTask.name,
+        description: newTask.description,
+        dueDate: new Date(newTask.dueDate),
+        completed: newTask.completed,
+    }).returning();
+
+    return NextResponse.json(createdTask, { status: 201});
+}
